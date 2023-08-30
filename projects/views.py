0ae9033ca91713864560927b97391fef1e67c1e5
Anurag Kumar <anuragabcr@gmail.com>
2023-08-30 16:14:38 +0530

    crud operations

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Project
+from .forms import ProjectForm
 
 
 def projects(request):
@@ -12,3 +12,37 @@ def project(request, pk):
     data = Project.objects.get(id=pk)
     tags = data.tags.all()
     return render(request, 'projects/project.html', {'project': data, 'tags': tags})
+
+
+def createProject(request):
+    form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
+
+
+def updateProject(request, pk):
+    data = Project.objects.get(id=pk)
+    form = ProjectForm(instance=data)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=data)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
+
+
+def deleteProject(request, pk):
+    data = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        data.delete()
+        return redirect('projects')
+    return render(request, 'projects/delete_object.html', {'object': data})
