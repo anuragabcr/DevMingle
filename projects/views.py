d648f3c1c871bdda739dc15f66c4880f7a337cfd
Anurag Kumar <anuragabcr@gmail.com>
2023-08-30 14:16:29 +0530

    basic model

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    data = Project.objects.all()
+    return render(request, 'projects/projects.html', {'projects': data})
 
 
 def project(request, pk):
-    return render(request, 'projects/project.html')
+    data = Project.objects.get(id=pk)
+    tags = data.tags.all()
+    return render(request, 'projects/project.html', {'project': data, 'tags': tags})
