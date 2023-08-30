dd13bfd465af74a3591b5fd8f968d7f5ca8acb4f
Anurag Kumar <anuragabcr@gmail.com>
2023-08-30 10:14:18 +0530

    start project

---
+++ devmingle/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for devmingle project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devmingle.settings')
+
+application = get_asgi_application()
